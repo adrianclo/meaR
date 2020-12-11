eb2df48783f06748375c2a3daf6efc33a8d79851
adrianclo <adrianclo1984@gmail.com>
2020-12-11 14:32:47 +0100

    update

--- muna-disentanglement/get-burst-activity-from-munas-files.R
+++ muna-disentanglement/get-burst-activity-from-munas-files.R
@@ -5,6 +5,7 @@ source("muna-disentanglement/mea_functions_v2.R")
 # MUNA FILES
 # dir <- "/Users/adrianlo/Desktop/MEA from Muna" # if on mac
 dir <- "C:/Users/Alo1/Documents/GitHub/meaR/muna-disentanglement/data" # if at work
+files_dir <- "C:/Users/Alo1/Documents/GitHub/meaR/muna-disentanglement/data" # if at work
 
 # meaTable <- readxl::read_excel("muna-disentanglement/meaTable.xlsx", sheet = "subset", range = "A1:I3")
 meaTable <- readxl::read_excel("muna-disentanglement/meaTable.xlsx", sheet = "full", range = "A1:I26")
@@ -25,3 +26,7 @@ ml_bursts <- burst_identifier(ml_filtered)
 # plot_spikes(ml_filtered, xlim = 1800, xticks = 300)
 plot_spikes(ml_bursts$spike_df, xlim = 1800, xticks = 300, burst_overlay = T)
 
+ml_bursts$spike_df %>%
+    filter(fileName %in% c("2017101114h05spikes 2.8.txt", "20180123-15h58spikes 2.8.txt")) %>% 
+    filter(!is.na(burst_id)) %>% 
+    plot_spikes(xlim = 1800, xticks = 300, burst_overlay = F)
